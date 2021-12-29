45ed14a41a2ca016dd78986e1c1a293bacff5d5b
boubou90 <34448542+boubou90@users.noreply.github.com>
2021-12-29 15:09:52 +0100

    Update settings.py

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -129,6 +129,10 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
 # My settings
 LOGIN_URL = 'users:login'
 
