e65f29dacb9782787c1986c9e51b38406fcac4ce
boubou90 <34448542+boubou90@users.noreply.github.com>
2021-12-29 18:04:10 +0100

    Update settings.py

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -129,6 +129,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
